75692ae49b918aee715006b2630896d26758ae3b
Spencer Hulse <hulse.spencer@gmail.com>
2022-02-07 04:17:20 -0500

    Added prompts for other employees (Engineers and Interns).

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const inquirer = require("inquirer");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
 const teamManagerPrompts = [
   {
@@ -30,7 +33,7 @@ const teamManagerPrompts = [
   {
     type: "input",
     name: "email",
-    message: "What is the team managers email?",
+    message: "What is the team manager's email?",
     validate: (emailInput) => {
       if (emailInput) {
         return true;
@@ -54,12 +57,128 @@ const teamManagerPrompts = [
     },
   },
 ];
-const otherMemberPrompts = [];
+
+const otherMemberPrompts = [
+  {
+    type: "list",
+    message: "What type of team member would you like to add?",
+    name: "member",
+    choices: ["Engineer", "Intern"],
+  },
+  {
+    type: "input",
+    name: "name",
+    message: "What is the employee's name?",
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log("Please enter a name.");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the employee's ID number?",
+    validate: (idInput) => {
+      if (idInput) {
+        return true;
+      } else {
+        console.log("Please enter an ID number.");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the employee's email?",
+    validate: (emailInput) => {
+      if (emailInput) {
+        return true;
+      } else {
+        console.log("Please enter an email address.");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "What is the name of the intern's school?",
+    when: ({ member }) => {
+      if (member === "Intern") {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    validate: (schoolInput) => {
+      if (schoolInput) {
+        return true;
+      } else {
+        console.log("Please enter a school.");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is the engineer's github username?",
+    when: ({ member }) => {
+      if (member === "Engineer") {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    validate: (githubInput) => {
+      if (githubInput) {
+        return true;
+      } else {
+        console.log("Please enter a github username.");
+        return false;
+      }
+    },
+  },
+  {
+    type: "confirm",
+    name: "confirmAddMember",
+    message: "Would you like to add another team member?",
+    default: false,
+  },
+];
 
 const initializePrompts = () => {
   return inquirer.prompt(teamManagerPrompts);
 };
 
-initializePrompts().then((data) => {
-  console.log(data);
-});
+const otherMembers = (teamData) => {
+  if (!teamData.members) {
+    teamData.members = [];
+  }
+
+  return inquirer.prompt(otherMemberPrompts).then((memberData) => {
+    teamData.members.push(memberData);
+    if (memberData.confirmAddMember) {
+      return otherMembers(teamData);
+    } else {
+      return teamData;
+    }
+  });
+};
+
+initializePrompts()
+  .then(otherMembers)
+  .then((teamData) => {
+    console.log(teamData);
+    /*   const teamManager = new Manager(
+    data.name,
+    data.id,
+    data.email,
+    data.officeNumber
+  ); */
+  });
