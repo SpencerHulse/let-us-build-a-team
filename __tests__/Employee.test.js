07b0cdb6b929d466d25f85330377d185c99118ef
Spencer Hulse <hulse.spencer@gmail.com>
2022-02-07 02:39:11 -0500

    Tested Employee properties and methods.

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,29 @@
+const Employee = require("../lib/Employee");
+
+test("creates an employee object", () => {
+  const employee = new Employee("Spencer", 1234, "adc123@gmail.com");
+
+  expect(employee.name).toBe("Spencer");
+  expect(employee.id).toBe(1234);
+  expect(employee.email).toBe("adc123@gmail.com");
+});
+
+test("gets an employee's name", () => {
+  const employee = new Employee("Spencer", 1234, "adc123@gmail.com");
+
+  expect(employee.getName()).toEqual(expect.stringContaining(employee.name));
+});
+
+test("gets an employee's id", () => {
+  const employee = new Employee("Spencer", 1234, "adc123@gmail.com");
+
+  expect(employee.getId()).toEqual(
+    expect.stringContaining(employee.id.toString())
+  );
+});
+
+test("gets an employee's email", () => {
+  const employee = new Employee("Spencer", 1234, "adc123@gmail.com");
+
+  expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email));
+});
